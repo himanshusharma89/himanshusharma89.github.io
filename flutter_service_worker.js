9c4a97ec488f2e9635d662771a0d9b5be2e7f37c
Himanshu Sharma <ranubhardwaj89@gmail.com>
2021-05-21 11:29:13 +0530

    Website Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,22 +3,34 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "b35a843697d2ef371a9ddca1363f336f",
-"assets/assets/about_me.png": "d5a888e37d42e45234e21938423ab64f",
-"assets/assets/about_me.svg": "f1f23a4a016d26bce947033e62254392",
+  "assets/AssetManifest.json": "bf8e624366286148c5f911e10bfb75ae",
 "assets/assets/github_icon.png": "472739dfb5857b1f659f4c4c6b4568d0",
-"assets/assets/placeholder.gif": "4bad0fdca4faa287bb97d6eca90dbeeb",
+"assets/assets/placeholder.gif": "8316981505329dc138cf38dbd351834d",
+"assets/assets/tech/amplify.png": "08d477c1fa9750a333a666505b138f78",
+"assets/assets/tech/azure.png": "8c044f9ab66570593c0465544518a365",
+"assets/assets/tech/c.png": "5080745a05edd274d01f49ac4d329eb7",
+"assets/assets/tech/dart.png": "300b451c547bdca8d526ec82e63ea52b",
+"assets/assets/tech/firebase.png": "b2c85c34cfe130cb19a06507654e534f",
+"assets/assets/tech/flutter.png": "1272c293af14af47352548e906e40e71",
+"assets/assets/tech/git.png": "728ff5a8e44d74cd0f2359ef0a9ec88a",
+"assets/assets/tech/github_action.png": "61625bd9c04a307a72b2681223a3e4ff",
+"assets/assets/tech/graphql.png": "501707a407255cedb68affe213aa308c",
+"assets/assets/tech/java.png": "a6e62e8811682b6cca79cd3928f5865b",
+"assets/assets/tech/js.png": "745456d8484e68cc411c189a28276d4c",
+"assets/assets/tech/markdown.png": "85a477796a34f83d65a2c6509959bf72",
+"assets/assets/tech/mongodb.png": "2dcbb345e0f0f316ffef52677e81185a",
+"assets/assets/tech/node.png": "288092c156378030d63054fd307c0b89",
 "assets/assets/wakatime_icon.png": "a7aa48a3891e7332db43271f3a15198e",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "1fe12171b401fe9374c74d18c836900f",
-"icons/heart_blue.png": "85ab16edaf50f647e657720b2c3df2b6",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/loader.gif": "ee45880dd9da258d02a758430a78c485",
-"index.html": "5bf4c0b951b91cb033b0a45f18238d35",
-"/": "5bf4c0b951b91cb033b0a45f18238d35",
-"main.dart.js": "204417df30c6243e32699831da3b0485",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/NOTICES": "bb8a7e6a6a739895ded1b3e245a28d68",
+"favicon.png": "f5ce0ba9c9c8cb72d85070df8dcafa14",
+"icons/Icon-192.png": "dbdb6f0b0cc9af8c1262a460cea17a5d",
+"icons/Icon-512.png": "7d9c79f2bdcc893da9c44fd0e93a418c",
+"icons/progressloading.json": "18b9eb1b6e9199482fe5fc4e9ba6ab66",
+"index.html": "040892baa610de4454bd9a5a3a383f65",
+"/": "040892baa610de4454bd9a5a3a383f65",
+"main.dart.js": "26abafb0a6c70adb879fa2eea803a7f1",
 "manifest.json": "1be9db6342f3c7d5d6ad928f61c39cf0",
 "styles.css": "ed47d53ae7d76b5888e943cdfb0c33c4",
 "version.json": "aaafa928a4f8616604eca12d830fe765"
@@ -39,7 +51,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
@@ -165,7 +177,7 @@ async function downloadOffline() {
     }
     currentContent[key] = true;
   }
-  for (var resourceKey in Object.keys(RESOURCES)) {
+  for (var resourceKey of Object.keys(RESOURCES)) {
     if (!currentContent[resourceKey]) {
       resources.push(resourceKey);
     }

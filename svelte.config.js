e3914f381505bb3ac147a1c46a04832f4339f42e
Himanshu Sharma <contact@himanshusharma.tech>
2025-07-06 01:30:06 +0530

    made static, let's retry

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,16 +6,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// if your repo is username.github.io, leave fallback and base as default
-			// if it's a project page, set base: '/repo-name'
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		paths: {
-			base: ''
-		}
+		adapter: adapter()
 	}
 };
 
